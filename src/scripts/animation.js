60def20a320d5c75c676692b0382ff846c2d010e
ckulwik <ckulwik@umich.edu>
2021-02-23 22:22:15 -0500

    fixing up the logo animation

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -9,27 +9,19 @@ const animateMainImage = () => {
     const halfWinWidth = windowWidth / 2;
     const halfWinHeight = windowHeight / 2;
 
-    const maxWiggleHorz = 15;
-    const maxWiggleVert = 15;
+    const maxWiggleHorz = 100;
+    const maxWiggleVert = 100;
 
-    const imgWidth = document.querySelector(".img-container").offsetWidth;
-    const imgHeight = document.querySelector(".img-container").offsetHeight;
-
-    const vertAdjustment = 150;
-    // clientX at 0 -> want to move img right 100
-    // clientX at width/2, no move 
-    // clientX at WinWidth -> move img left 100
-
-    // const horizPos = halfWinWidth + maxWiggleHorz((e.clientX - halfWinWidth) / halfWinWidth)
+    const vertAdjustment = 0;
 
     root.addEventListener("mousemove", (e) => {
         const horizPercentAdj = (halfWinWidth - e.clientX) / halfWinWidth;
-        const horizPos = halfWinWidth + maxWiggleHorz * horizPercentAdj;
-        ckulwikImg.style.left = horizPos - imgWidth / 1.5 + "px";
+        const horizPos = maxWiggleHorz * horizPercentAdj;
+        ckulwikImg.style.marginLeft = horizPos + "px";
 
         const vertPercentAdj = (halfWinHeight - e.clientY) / halfWinHeight;
-        const vertPos = halfWinHeight + maxWiggleVert * vertPercentAdj;
-        ckulwikImg.style.top = vertPos - imgHeight / 1.5 + vertAdjustment + "px";
+        const vertPos = maxWiggleVert * vertPercentAdj;
+        ckulwikImg.style.marginTop = vertPos + vertAdjustment + "px";
     });
 }
 
