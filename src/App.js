eca9598bf7cea3b9fe998755f08b1388bf7eb4de
Chris Kulwik <ckulwik@umich.edu>
2021-05-13 17:06:48 -0400

    finising the merge

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ const App = () => {
   return (
     <>
       <div className="content-container">
-<<<<<<< HEAD
         <h1>Will the title change????</h1>
-=======
-        <h1>Hey Keelers</h1>
->>>>>>> master
         <div className="menu-container"></div>
 
         <div className="page" />
