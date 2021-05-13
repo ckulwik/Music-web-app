b914e47fecfc9f963b4ab7c255e7831cf759a7ba
ckulwik <ckulwik@umich.edu>
2021-05-13 17:07:17 -0400

    Actions demo setup (#12)
    * removing junk
    
    * default title
    
    * finising the merge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <div className="content-container">
-        <h1>Hey Keelers</h1>
+        <h1>Will the title change????</h1>
         <div className="menu-container"></div>
 
         <div className="page" />
