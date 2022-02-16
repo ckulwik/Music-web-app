958e46e5d3db78ea492ad9f72098e87b835fa6fa
Chris Kulwik <ckulwik@umich.edu>
2022-02-16 12:05:45 -0500

    soundcloud embedding

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -2,7 +2,6 @@ import "../styles/section.css";
 import SongCard from "./SongCard";
 
 const Music = () => {
-
   return (
     <div
       className="page-container music-page-container"
@@ -11,12 +10,35 @@ const Music = () => {
       data-aos-delay="100"
       data-aos-once
     >
-      <SongCard
-        coverImgSrc="https://music-web-app-songs.s3.us-east-2.amazonaws.com/ypsibridgecoverpng.png"
-        coverImgAlt="cover for chris kulwik's 'Ypsi Bridge' song"
-        songTitle="Ypsi Bridge"
-        audioSrc="https://music-web-app-songs.s3.us-east-2.amazonaws.com/ypsiFinalMaybe3.wav"
-      />
+      <SongCard>
+        <iframe
+          width="100%"
+          height="300"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1172045620&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+        <div className="ypsi-bridge-div">
+          <a
+            href="https://soundcloud.com/chris-kulwik"
+            title="Chris Kulwik"
+            target="_blank"
+            className="ypsi-bridge-a"
+          >
+            Chris Kulwik
+          </a>
+          ·
+          <a
+            href="https://soundcloud.com/chris-kulwik/ypsi-bridge"
+            title="Ypsi Bridge"
+            target="_blank"
+            className="ypsi-bridge-a"
+          >
+            Ypsi Bridge
+          </a>
+        </div>
+      </SongCard>
     </div>
   );
 };
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,49 +1,51 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 import "../styles/songCard.css";
 
-const SongCard = ({ songTitle, coverImgSrc, coverImgAlt, audioSrc }) => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 700px)",
-  });
+const SongCard = ({ children }) => {
+  // const isDesktop = useMediaQuery({
+  //   query: "(min-width: 700px)",
+  // });
   return (
-    <>
-      {isDesktop ? (
-        <div className="card-container desktop">
-          <img
-            className="card-img desktop"
-            alt={coverImgAlt}
-            src={coverImgSrc}
-          />
-          <div className="title-and-player">
-            <div className="title-container desktop">
-              <h3>{songTitle}</h3>
-            </div>
-            <audio
-              controls
-              controlsList="nodownload noplaybackrate"
-              className="audio-player desktop"
-            >
-              <source src={audioSrc} type="audio/wav" />
-              Your browser does not support the audio element.
-            </audio>
-          </div>
-        </div>
-      ) : (
-        <div className="card-container">
-          <div className="image-and-title">
-            <img className="card-img" alt={coverImgAlt} src={coverImgSrc} />
-            <div className="title-container">
-              <h3>{songTitle}</h3>
-            </div>
-          </div>
-          <audio controls controlsList="nodownload" className="audio-player">
-            <source src={audioSrc} type="audio/wav" />
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      )}
-    </>
+    <div className="card-container">{children}</div>
+
+    // <>
+    //   {isDesktop ? (
+    //     <div className="card-container desktop">
+    //       <img
+    //         className="card-img desktop"
+    //         alt={coverImgAlt}
+    //         src={coverImgSrc}
+    //       />
+    //       <div className="title-and-player">
+    //         <div className="title-container desktop">
+    //           <h3>{songTitle}</h3>
+    //         </div>
+    //         <audio
+    //           controls
+    //           controlsList="nodownload noplaybackrate"
+    //           className="audio-player desktop"
+    //         >
+    //           <source src={audioSrc} type="audio/wav" />
+    //           Your browser does not support the audio element.
+    //         </audio>
+    //       </div>
+    //     </div>
+    //   ) : (
+    //     <div className="card-container">
+    //       <div className="image-and-title">
+    //         <img className="card-img" alt={coverImgAlt} src={coverImgSrc} />
+    //         <div className="title-container">
+    //           <h3>{songTitle}</h3>
+    //         </div>
+    //       </div>
+    //       <audio controls controlsList="nodownload" className="audio-player">
+    //         <source src={audioSrc} type="audio/wav" />
+    //         Your browser does not support the audio element.
+    //       </audio>
+    //     </div>
+    //   )}
+    // </>
   );
 };
 
