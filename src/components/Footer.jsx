0aca6df514fc114c6107e6b72dbaeeacfcbaa067
Chris Kulwik <ckulwik@umich.edu>
2023-06-16 11:17:14 -0400

    adding 3 new songs, changing grid max width, add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-circle" />
+      <div className="footer-circle-crop" />
+      <div className="footer-hide-headers" />
+      <div className="footer-top-lines" />
+      <div className="yellow-line"/>
+      <div className="orange-line"/>
+      <div className="red-line"/>
+    </div>
+  );
+};
+
+export default Footer;
