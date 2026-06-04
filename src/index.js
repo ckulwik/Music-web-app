c8458227bef67a1e2f460ff48a667f71d1480b88
Chris Kulwicki <ckulwik@gmail.com>
2026-06-04 16:27:18 -0400

    add tailwind

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
+import './index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
